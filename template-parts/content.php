b7eacccb2cdfcadd4e3ca910bc97c8ccf9dadd9a
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-01-15 22:40:14 -0500

    Updated blog archive markup

--- template-parts/content.php
+++ template-parts/content.php
@@ -16,32 +16,14 @@
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php pt_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
 		endif; ?>
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'pt' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+		
+		<?php the_excerpt(); ?>
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pt' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php pt_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->

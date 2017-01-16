b677e4be064a30754a913c3048acc84eebce62f7
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-01-15 21:34:00 -0500

    Added about and contact page templates

---
+++ templates/contact.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * TEMPLATE NAME: Contact
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package port
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post(); ?>
+
+				<div class="row section-padding">
+					<div class="columns small-12">
+						<?php the_content(); ?>
+					</div>
+				</div>
+
+
+			<?php endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();

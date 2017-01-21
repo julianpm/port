5b7fab1d5d384f5a55d4b54ca75f61d1ee1b4ee8
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-01-21 17:55:03 -0500

    wrote flyout nav js functionality

--- gulp/src/js/app.js
+++ gulp/src/js/app.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function($){
 
-	
+	$('.flyout-toggle').click(function(){
+		$('.flyout').animate({
+			top: 0
+		}, 200, function(){
+		});
+	});
+
+	$('.flyout-toggle-close').click(function(){
+		$('.flyout').animate({
+			top:-190
+		}, 200, function(){
+		});
+	});
 
 });

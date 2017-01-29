47a623bad13f3220898bf83ed811e0a7116f4e65
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-01-28 23:18:55 -0500

    Adjusted markup of archive.php

--- archive.php
+++ archive.php
@@ -13,33 +13,29 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
+		if ( have_posts() ) :
 
-			<header class="page-header">
+			pt_post_header(); ?>
+			
+			<section class="row section-padding">
+				
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				/* Start the Loop */
+				while ( have_posts() ) : the_post(); ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					<div class="columns small-12 large-4">
+						
+						<?php get_template_part( 'template-parts/content', 'projects' ); ?>
+						
+					</div>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				<?php
+				endwhile; ?>
+			
+			</section>
 
-			endwhile;
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
+			<?php the_posts_navigation();
 
 		endif; ?>
 
@@ -47,5 +43,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();

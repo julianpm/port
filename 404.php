dce3a28afe88db845129a3e2ba6dde845d51c023
Julian Meanchoff <jpmeanchoff@gmail.com>
2017-01-31 09:24:26 -0500

    built 404

--- 404.php
+++ 404.php
@@ -13,43 +13,12 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header>
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'pt' ); ?></h1>
+				<header class="page-header-simple">
+					<h1 class="page-title"><?php esc_html_e( '404', 'pt' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pt' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( pt_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'pt' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-					
-					?>
-
+				<div class="page-content section-padding">
+					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'pt' ); ?></p>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
